f4722c70742bc89bb5548832de7cf2074e34fd9b
Maya Thompson <majsant96@hotmail.com>
2025-09-29 13:30:35 +0200

    add middleware folder and add validation for authdata

--- express-api/src/routes/auth.ts
+++ express-api/src/routes/auth.ts
@@ -1,7 +1,13 @@
 import express from "express";
-import type { Request, Response } from "express";
 import { pool } from "../database.js";
 import type { ResultSetHeader } from "mysql2/promise";
 import type { User } from "../interfaces.js";
+import { validateAuthData } from "../middleware/validation.js";
 
 const router = express.Router();
+
+router.post("/register", validateAuthData, async (req, res) => {});
+
+router.post("/login", validateAuthData, async (req, res) => {});
+
+export default router;
