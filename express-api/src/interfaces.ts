d9b5374b3735476e86379759c7c7158504519bef
Maya Thompson <majsant96@hotmail.com>
2025-09-29 13:10:29 +0200

    add interface file with user, article and articlewithuser interfaces

---
+++ express-api/src/interfaces.ts
@@ -0,0 +1,19 @@
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+}
+
+export interface Article {
+  id: number;
+  title: string;
+  body: string;
+  category: string;
+  user_id: number;
+  created_at?: Date;
+}
+
+export interface ArticleWithUser extends Article {
+  username: string;
+  email: string;
+}
--- express-api/src/routes/auth.ts
+++ express-api/src/routes/auth.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import type { Request, Response } from "express";
 import { pool } from "../database.js";
+import type { ResultSetHeader } from "mysql2/promise";
+import type { User } from "../interfaces.js";
 
 const router = express.Router();
